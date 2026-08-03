0e4dd69246bcb9a00ba103d06f94aaa016a0df76
somnathhake09 <somnathhake09@gmail.com>
2026-08-03 16:45:51 +0530

    add a changes in stud info project

--- stud_info.py
+++ stud_info.py
@@ -3,4 +3,7 @@ class Student:
         self.name = name
         self.roll_no = roll_no
         self.marks = marks  # list of 3 marks
-        
+    def calculate_percentage(self):
+        total = sum(self.marks)
+        percentage = total / len(self.marks)
+        return percentage
