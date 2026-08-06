840252623b575a862a612a2a2f4bed814875ff80
somnathhake09 <somnathhake09@gmail.com>
2026-08-06 16:49:39 +0530

    add a stud info mini project in changes add a display info functions to display student inormation

--- stud_info.py
+++ stud_info.py
@@ -2,11 +2,13 @@ class Student:
     def __init__(self, name, roll_no, marks):
         self.name = name
         self.roll_no = roll_no
-        self.marks = marks  # list of 3 marks
+        self.marks = marks
+
     def calculate_percentage(self):
         total = sum(self.marks)
         percentage = total / len(self.marks)
         return percentage
+
     def calculate_grade(self):
         percentage = self.calculate_percentage()
         
@@ -20,3 +22,19 @@ class Student:
             return "C"
         else:
             return "Fail"
+
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"Roll No: {self.roll_no}")
+        print(f"Marks: {self.marks}")
+        print(f"Percentage: {self.calculate_percentage():.2f}%")
+        print(f"Grade: {self.calculate_grade()}")
+        print("-" * 30)
+
+
+# Objects banवून test karूया
+s1 = Student("Shivaji", 1, [90, 85, 95])
+s2 = Student("Sambhaji", 2, [60, 55, 70])
+
+s1.display_info()
+s2.display_info()
