46dff3344cd36bd334b4534e890b41b60bc58326
somnathhake09 <somnathhake09@gmail.com>
2026-08-21 15:25:39 +0530

    add a new python project using basic python to create a student profile throught user

---
+++ stud_profile.py
@@ -0,0 +1,19 @@
+"""=====================
+      Student Profile
+   ====================="""
+
+name = input("Enter Your Name :-")
+age = int(input("Enter Your Age :-"))
+college = input("Enter Your College Name :-")
+course = input("Enter Your Course Name :-")
+city = input("Enter Your City Name :-")
+percentage = float(input("Enter Your Percentage :-"))
+
+print("********** Student Profile **********")
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Course:", course)
+print("City:", city)
+print("Percentage:", percentage)
+print("*************************************")
